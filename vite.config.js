666780bb0d2322deb45132606bec320e8cc4169f
Thomas Paulus <thomas.paulus@eme.com>
2026-08-20 14:32:42 +0200

    Repo eingetragen als base in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ export default defineConfig({
   // WICHTIG: hier muss der Name eures GitHub-Repos stehen, mit Schrägstrichen.
   // Beispiel: Repo "vokabelkiste"  ->  base: '/vokabelkiste/'
   // Ohne das findet GitHub Pages die Dateien nicht.
-  base: '/HIER-REPO-NAME-EINTRAGEN/',
+  base: '/VocApp/',
 });
